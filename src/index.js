223e447725e4e2ec30f4c8e0b6dbfdfcc13e0e8b
dijon64 <john1.mclean@outlook.com>
2022-08-10 20:27:54 -0400

    update readme

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+continue at 'Developer tools' header
+
 */
 class Square extends React.Component {
   constructor(props) {
